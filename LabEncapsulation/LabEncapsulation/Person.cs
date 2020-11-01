540a73c41561a22f76c1a88b298c67e512f98208
Ventsislava V Genova <73789010+moontino@users.noreply.github.com>
2020-11-01 18:44:59 +0200

    Add files via upload

---
+++ LabEncapsulation/LabEncapsulation/Person.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private const string INVALID_MESS_NAME = "name cannot contain fewer than 3 symbols!";
+        private const int MINIMAL_SALARY = 460;
+
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException($"First {INVALID_MESS_NAME}");
+                }
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException($"Last {INVALID_MESS_NAME}");
+                }
+                this.lastName = value;
+            }
+        }
+        public int Age
+        {
+            get { return this.age; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                this.age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get
+            { return this.salary; }
+            private set
+            {
+                if (value < MINIMAL_SALARY)
+                {
+                    throw new ArgumentException($"Salary cannot be less than {MINIMAL_SALARY} leva!");
+                }
+                this.salary = value;
+            }
+        }
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                percentage = percentage / 2;
+            }
+            this.Salary *= 1 + percentage / 100;
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} gets {this.Salary:f2} leva.";
+        }
+    }
+}
---
+++ LabEncapsulation/LabEncapsulation/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+            
+            for (int i = 0; i < lines; i++)
+            {
+                string[] personArg = Console.ReadLine().Split(" ").ToArray();
+                string firstName = personArg[0];
+                string lastName = personArg[1];
+                int age = int.Parse(personArg[2]);
+                decimal salary = decimal.Parse(personArg[3]);
+                Person person = null;
+                try
+                {
+                    person = new Person(firstName, lastName, age, salary);
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                people.Add(person);
+               // team.AddPlayer(person);
+            }
+            string teamName = Console.ReadLine();
+            Team team = new Team(teamName);
+            foreach (Person person in people)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
